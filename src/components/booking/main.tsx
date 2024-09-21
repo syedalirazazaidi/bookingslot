eac091c2c2771dd73912642489f1c67b1e0e5fee
Ali Raza <aaliraza007@hotmail.com>
2024-09-21 15:45:52 +0500

    Added stat section with shadow effect, hover animations, and refactored layout using Ant Design

--- src/components/booking/main.tsx
+++ src/components/booking/main.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Herosection from './HeroSection'
+import Stats from './Stats'
 
 export default function Booking() {
     return (
         <div>
             <Herosection/>
+            <Stats/>
         </div>
     )
 }
