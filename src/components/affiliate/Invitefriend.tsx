c7a8720edd3aab2b8edb9aa1331e0a7645b2e569
Ali Raza <aaliraza007@hotmail.com>
2024-09-23 19:55:14 +0500

    added new component

--- src/components/affiliate/Invitefriend.tsx
+++ src/components/affiliate/Invitefriend.tsx
@@ -1,153 +1,78 @@
 import React from 'react';
+import { StaticImage } from 'gatsby-plugin-image'; // Import StaticImage from gatsby
+import { Row, Col } from 'antd';
 
 export default function Invitefriend() {
   return (
-    <section className="text-gray-600 body-font">
-      <div className=" px-24 py-24  flex flex-wrap">
-        <div className="flex flex-wrap w-full">
-          <img
-            className="lg:w-3/5 md:w-1/2 object-cover object-center rounded-lg md:mt-0 mt-12"
-            src="https://dummyimage.com/1200x500"
-            alt="step"
-          />
-          <div className="lg:w-2/5 md:w-1/2  md:py-6">
-            <div className="flex relative pb-12">
-              <div className="h-full w-10 absolute inset-0 flex items-center justify-center">
-                <div className="h-full w-1 bg-gray-200 pointer-events-none" />
-              </div>
-              <div className="flex-shrink-0 w-10 h-10 rounded-full bg-indigo-500 inline-flex items-center justify-center text-white relative z-10">
-                <svg
-                  fill="none"
-                  stroke="currentColor"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth={2}
-                  className="w-5 h-5"
-                  viewBox="0 0 24 24"
-                >
-                  <path d="M12 22s8-4 8-10V5l-8-3-8 3v7c0 6 8 10 8 10z" />
-                </svg>
-              </div>
-              <div className="flex-grow pl-4">
-                <h2 className="font-medium title-font text-sm text-gray-900 mb-1 tracking-wider">
-                  STEP 1
-                </h2>
-                <p className="leading-relaxed">
-                  VHS cornhole pop-up, try-hard 8-bit iceland helvetica. Kinfolk
-                  bespoke try-hard cliche palo santo offal.
-                </p>
-              </div>
+    <section className="text-gray-600 body-font bg-[#362C62]">
+      <div className="px-24 py-12">
+        <Row className="flex-wrap">
+          {/* Image Column */}
+          <Col xs={24} md={15}>
+            <StaticImage
+              src="../../images/affi-purple-bg.webp"
+              alt="hero-img"
+              placeholder="blurred"
+              layout="constrained"
+              className="object-cover object-center rounded"
+            />
+          </Col>
+
+          {/* Content Column */}
+          <Col xs={24} md={9} className='md:-ml-20'>
+            <div className="md:py-8">
+              <h1 className="text-yellow-400 text-2xl  font-medium mb-4">
+                You get 500 INR when your friend uses Easy Slot Booking via your
+                shared link.
+              </h1>
+              <ul className="list-none space-y-6">
+                <li>
+                  <div className="flex items-center mb-2">
+                    <span className="text-indigo-500 text-xl font-bold mr-2">
+                      01.
+                    </span>
+                    <span className="text-lg font-semibold text-white">
+                      Invite friends with your link
+                    </span>
+                  </div>
+                  <span className="text-base text-white">
+                    Share your personal link however you like - email it, post
+                    it, tweet it - so your friend can apply for a student loan.
+                  </span>
+                </li>
+                <li>
+                  <div className="flex items-center mb-2">
+                    <span className="text-indigo-500 text-xl font-bold mr-2">
+                      02.
+                    </span>
+                    <span className="text-lg font-semibold text-white">
+                      Get a cashback reward
+                    </span>
+                  </div>
+                  <span className="text-base text-white">
+                    If eligible, you and your Friend will receive a 500 INR
+                    check 30 days on or after the first disbursement on your
+                    Friend's first appointment booking.
+                  </span>
+                </li>
+                <li>
+                  <div className="flex items-center mb-2">
+                    <span className="text-indigo-500 text-xl font-bold mr-2">
+                      03.
+                    </span>
+                    <span className="text-lg font-semibold text-white">
+                      Refer more friends
+                    </span>
+                  </div>
+                  <span className="text-base text-white">
+                    Invite others and earn up to 2000 INR in cash rewards every
+                    year!
+                  </span>
+                </li>
+              </ul>
             </div>
-            <div className="flex relative pb-12">
-              <div className="h-full w-10 absolute inset-0 flex items-center justify-center">
-                <div className="h-full w-1 bg-gray-200 pointer-events-none" />
-              </div>
-              <div className="flex-shrink-0 w-10 h-10 rounded-full bg-indigo-500 inline-flex items-center justify-center text-white relative z-10">
-                <svg
-                  fill="none"
-                  stroke="currentColor"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth={2}
-                  className="w-5 h-5"
-                  viewBox="0 0 24 24"
-                >
-                  <path d="M22 12h-4l-3 9L9 3l-3 9H2" />
-                </svg>
-              </div>
-              <div className="flex-grow pl-4">
-                <h2 className="font-medium title-font text-sm text-gray-900 mb-1 tracking-wider">
-                  STEP 2
-                </h2>
-                <p className="leading-relaxed">
-                  Vice migas literally kitsch +1 pok pok. Truffaut hot chicken
-                  slow-carb health goth, vape typewriter.
-                </p>
-              </div>
-            </div>
-            <div className="flex relative pb-12">
-              <div className="h-full w-10 absolute inset-0 flex items-center justify-center">
-                <div className="h-full w-1 bg-gray-200 pointer-events-none" />
-              </div>
-              <div className="flex-shrink-0 w-10 h-10 rounded-full bg-indigo-500 inline-flex items-center justify-center text-white relative z-10">
-                <svg
-                  fill="none"
-                  stroke="currentColor"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth={2}
-                  className="w-5 h-5"
-                  viewBox="0 0 24 24"
-                >
-                  <circle cx={12} cy={5} r={3} />
-                  <path d="M12 22V8M5 12H2a10 10 0 0020 0h-3" />
-                </svg>
-              </div>
-              <div className="flex-grow pl-4">
-                <h2 className="font-medium title-font text-sm text-gray-900 mb-1 tracking-wider">
-                  STEP 3
-                </h2>
-                <p className="leading-relaxed">
-                  Coloring book nar whal glossier master cleanse umami. Salvia
-                  +1 master cleanse blog taiyaki.
-                </p>
-              </div>
-            </div>
-            <div className="flex relative pb-12">
-              <div className="h-full w-10 absolute inset-0 flex items-center justify-center">
-                <div className="h-full w-1 bg-gray-200 pointer-events-none" />
-              </div>
-              <div className="flex-shrink-0 w-10 h-10 rounded-full bg-indigo-500 inline-flex items-center justify-center text-white relative z-10">
-                <svg
-                  fill="none"
-                  stroke="currentColor"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth={2}
-                  className="w-5 h-5"
-                  viewBox="0 0 24 24"
-                >
-                  <path d="M20 21v-2a4 4 0 00-4-4H8a4 4 0 00-4 4v2" />
-                  <circle cx={12} cy={7} r={4} />
-                </svg>
-              </div>
-              <div className="flex-grow pl-4">
-                <h2 className="font-medium title-font text-sm text-gray-900 mb-1 tracking-wider">
-                  STEP 4
-                </h2>
-                <p className="leading-relaxed">
-                  VHS cornhole pop-up, try-hard 8-bit iceland helvetica. Kinfolk
-                  bespoke try-hard cliche palo santo offal.
-                </p>
-              </div>
-            </div>
-            <div className="flex relative">
-              <div className="flex-shrink-0 w-10 h-10 rounded-full bg-indigo-500 inline-flex items-center justify-center text-white relative z-10">
-                <svg
-                  fill="none"
-                  stroke="currentColor"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth={2}
-                  className="w-5 h-5"
-                  viewBox="0 0 24 24"
-                >
-                  <path d="M22 11.08V12a10 10 0 11-5.93-9.14" />
-                  <path d="M22 4L12 14.01l-3-3" />
-                </svg>
-              </div>
-              <div className="flex-grow pl-4">
-                <h2 className="font-medium title-font text-sm text-gray-900 mb-1 tracking-wider">
-                  FINISH
-                </h2>
-                <p className="leading-relaxed">
-                  Pitchfork ugh tattooed scenester echo park gastropub whatever
-                  cold-pressed retro.
-                </p>
-              </div>
-            </div>
-          </div>
-        </div>
+          </Col>
+        </Row>
       </div>
     </section>
   );
