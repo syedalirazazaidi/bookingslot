2a345d89ed831d8a55d96b5b559a26eba16e7687
Osama4488 <syed.osama.ali.96@gmail.com>
2024-09-24 00:16:59 +0500

    pricing-page web view few design things remaining and mob responsove

--- src/components/pricing/Main.tsx
+++ src/components/pricing/Main.tsx
@@ -1,10 +1,17 @@
 import React from 'react';
 import HeroSection from './HeroSection';
 import TestimonialSection from './TestimonialSection';
+import Faq from './Faq';
+import PricingBookingSection from './PricingBooking';
+
 export default function Pricing() {
   return (
     <div>
       <HeroSection />
+      <PricingBookingSection />
+
+      
+      <Faq />
       <TestimonialSection />
     </div>
   );
